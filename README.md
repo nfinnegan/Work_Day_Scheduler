015ca2093016762b74c357644d1be1a8b003395a
Natalie Finnegan <nfinneg2@gmail.com>
2021-03-06 14:08:28 -0700

    few tweaks to JS & finished readme

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Work_Day_Scheduler
+
+## Project Overview
+For this application I was tasked with creating a Work Day Scheduler. While I made a few minor tweaks to the CSS, the bulk of the HTML & CSS were provided. 
+
+## Description
+In the 5th week of bootcamp we spent our time learning about Third-Party-APIs, this really unlocked a lot of tools available to developers in order to streamline workflows and improve effiencies. I capitlized on third party APIs, by using jQuery almost exclusively along with boostrap for my formatting. 
+
+## User Story
+```md
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+```md
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## Deployed Application
+Link to deployed application: https://nfinnegan.github.io/Work_Day_Scheduler/
+
+## Preview
+
+![Work_Day_Scheduler](Work_Day_Scheduler.gif)
+
--- script.js
+++ script.js
@@ -29,48 +29,47 @@ var plannerTimes = [
     },
 ];
 
+//setting a variable for current time so I can compare it to each time on my planner in order to add the past,present,future classes
 let currentTime = moment().format("k")
 let currentTimeAsNum = Number(currentTime);
 
 
 date();
+//setting current day / date to display
 function date(){
     today = moment().format("dddd, MMMM Do YYYY");
     $("#currentDay").text(today);
 };
 
 
-//creating 1 set of the rows & columns needed per time block
+//creating 1 set of the rows & columns needed per time block, BCS learning assistant helped me with the concept of passing el in my for each function to get the values i wanted
 plannerTimes.forEach(function(el){
     let plannerRow = $("<div></div>").addClass("row justify-content-center").appendTo(".container");
     let plannerCol1 = $("<div></div>").addClass("col-sm-2 col-md-2 col-lg-2 column1").appendTo(plannerRow).text(el.hour).attr("id", el.hour);
     let plannerCol2 = $("<input type='text' name='textbox' maxlength='140'></input>").addClass("col-sm-9 col-md-9 col-lg-9 column2 inputTasks ").attr("id", el.military).appendTo(plannerRow)
-    let saveBtnCol3 = $("<i><i/>").addClass("col-sm-1 col-md-1 col-lg-1 column3 saveBtn far fa-save").appendTo(plannerRow);
+    $("<i><i/>").addClass("col-sm-1 col-md-1 col-lg-1 column3 saveBtn far fa-save").appendTo(plannerRow);
     
 
-    //adding past, present, future class for proper input box coloring based on current time
+    //adding past, present, future class for each input box based on current time
 
         if (currentTimeAsNum > el.military) {
             plannerCol2.addClass("past");
-        }
+            }
         else if (currentTimeAsNum < el.military) {
             plannerCol2.addClass("future");
-        }
+            }
         else  {
             plannerCol2.addClass("present");
-        };
+            };
         
         
         //adding AM or PM respectively
         if(el.hour === "9:00" || el.hour === "10:00" || el.hour === "11:00"){
-           var timeCol = plannerCol1.text(el.hour + "AM");
+           plannerCol1.text(el.hour + "AM");
             }
         else{
-           var timeCol = plannerCol1.text(el.hour + "PM")
+           plannerCol1.text(el.hour + "PM")
             }
-
-
-
 
    
 }) 
@@ -85,6 +84,7 @@ $(document).on("click", ".saveBtn",function(event){
       
     })
 
+//getting locally stored items and displaying them on the page    
 function getStorage() {
     $("#9").val(localStorage.getItem("9"));
     $("#10").val(localStorage.getItem("10"));
